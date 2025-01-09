2a4786ae73075fa44d21ab12f1c1cac7fb5ee11c
VajrapuKeerthana <2211cs010584@mallareddyuniversity.ac.in>
2025-01-09 11:50:09 +0530

    demo1

---
+++ functions.js
@@ -0,0 +1,54 @@
+// ! Functions
+// function demo(){
+//     console.log("function without parameters");
+// }
+// demo();
+
+// ! function with parameters
+// function userDetails(name,age,city){
+//     console.log(name);
+//     console.log(age);
+//     console.log(city);
+// }
+// ! calling the function
+// userDetails("Komal",20,"konda");
+
+// ! Anonymous function
+// function(){
+//     console.log("Anonymous function");
+// }
+// ();
+
+// ! function expression
+// var x=function(name,age,city){
+//     console.log(name);
+//     console.log(age);
+//     console.log(city);
+// }
+// x("Komal",20,"errasamudhram");
+
+// ! IIFE
+// (function(){
+//     console.log("IIFE");
+// })();
+
+// ! Arrow function
+// const x=()=>{
+//     console.log("Arrow function");
+// }
+// x();
+
+// const x=_=>console.log("Arrow function");
+// x();
+
+// const x=a=>console.log(a);
+// x(5);
+
+// const x=(a,b)=>console.log(a+b);
+// x(5,5);
+
+const x=_=>{console.log("Hi");
+    console.log("Hello");
+    console.log("bye");
+}
+x();
