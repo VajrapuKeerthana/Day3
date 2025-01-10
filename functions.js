0933c9bc3b074fc7d985f9c603f81567a2cc59d1
VajrapuKeerthana <2211cs010584@mallareddyuniversity.ac.in>
2025-01-10 10:26:30 +0530

    demo1

--- functions.js
+++ functions.js
@@ -47,8 +47,49 @@
 // const x=(a,b)=>console.log(a+b);
 // x(5,5);
 
-const x=_=>{console.log("Hi");
-    console.log("Hello");
-    console.log("bye");
+//const x=_=>{console.log("Hi");
+  //  console.log("Hello");
+    //console.log("bye");
+//}
+//x();
+// ! return Keyword
+function demo(a,b){
+    return a+b;
+};
+let x = demo(5,5);
+console.log(x);
+
+let y = (a,b) => a+b;//! implict return
+let a= y(10,20);
+console.log(a);
+
+let z = (a,b) => a+b};//! explict return
+let b = z(10,100);
+console.log(b);
+
+//! Higher order function
+
+function hof(a){
+      return a;
+};
+let x = hof(function(){return "Hello i am a callback function"});
+console.log(x);
+
+function parent(callback){
+     return callback
+
+let x = parent(add(5,5));
+return a+b
 }
+
+// ! closure
+function x(){
+      var user = "Vinay";
+      let company = "Google";
+      const sal = 1234567890;
+      console.log(user);
+      console.log(company);
+      console.log(sal);
+      console.log(a ,b);
+};
 x();
